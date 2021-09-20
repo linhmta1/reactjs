00640f9e9c672d8845506d542047b61be793fafe
NguyenVanLinh <linhmta@gmail.com>
2021-09-20 08:52:39 +0700

    fix ex2: inline js to addEventListener

--- 02_dice_html/02_dice_html/main.js
+++ 02_dice_html/02_dice_html/main.js
@@ -1,6 +1,10 @@
 var dice1 = document.getElementById("dice-1");
 var dice2 = document.getElementById("dice-2");   
+const rollBt= document.querySelector('.roll-dice');
+
 function handlerClick(evt){ 
    dice1.src="./images/dice"+  Math.floor((Math.random() * 6) + 1) + ".png";
    dice2.src="./images/dice"+  Math.floor((Math.random() * 6) + 1) + ".png";
 }
+
+rollBt.addEventListener('click', handlerClick);
